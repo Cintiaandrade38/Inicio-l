19f8f8927dca17ed8757fa4e90e32514412ac85f
Cintiaandrade38 <cintiadany28@gmail.com>
2025-01-30 21:26:13 -0300

    1º Projeto
    - Descobrir o nº secreto.

---
+++ app.js
@@ -0,0 +1,9 @@
+alert('Boas vindas  ao jogo do numero secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 e 10');
+
+if (numeroSecreto == chute) {
+    console.log('Isso ai,voce descobriu o numero secreto (5)');
+}  
+let mensagemDeBoasVindas = 'Boas vindas ao App';
+alert(mensagemDeBoasVindas);
